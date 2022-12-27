274b002ba0141c1c0b1a9dc6e8b377ed2075df43
Ryan Eby <ebyr@aadl.org>
2022-12-27 13:13:13 -0500

    first pass
    - remove settings
    - add task via url
    - toggle task via omnijs

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, EditorPosition, Plugin, PluginSettingTab, Setting, Notice } from 'obsidian';
+import { App, Editor, MarkdownView, EditorPosition, Notice, Plugin } from 'obsidian';
 
 function getCurrentLine(editor: Editor, view: MarkdownView) {
 	const lineNumber = editor.getCursor().line
@@ -8,18 +8,7 @@ function getCurrentLine(editor: Editor, view: MarkdownView) {
 
 interface TodoInfo {
 	title: string,
-	tags: string,
 	date: string
-}
-
-interface PluginSettings {
-	authToken: string,
-	defaultTags: string
-}
-
-const DEFAULT_SETTINGS: PluginSettings = {
-	authToken: '',
-	defaultTags: ''
 }
 
 function urlEncode(line: string) {
@@ -27,15 +16,13 @@ function urlEncode(line: string) {
 	return line
 }
 
-function contructTodo(line: string, settings: PluginSettings, fileName: string){
+function contructTodo(line: string, fileName: string){
 	line = line.trim();
-	const tags = extractTags(line, settings.defaultTags);
 
 	line = line.replace(/#([^\s]+)/gs, '');
 
 	const todo: TodoInfo = {
 		title: extractTitle(line),
-		tags: tags,
 		date: extractDate(fileName)
 	}
 
@@ -59,29 +46,16 @@ function extractTitle(line: string) {
 	if (content != null) {
 		title = content[0]
 	}
-	
+
 	return title;
 }
 
-function extractTags(line: string, setting_tags: string){
-	const regex = /#([^\s]+)/gs
-	const array = [...line.matchAll(regex)]
-	const tag_array = array.map(x => x[1])
-	if (setting_tags.length > 0) {
-		tag_array.push(setting_tags);
-	}
-	line = line.replace(regex, '');
-	const tags = tag_array.join(',')
-	
-	return tags;
-}
-
 function extractTarget(line: string) {
-	const regexId = /id=(\w+)/
+	const regexId = /task\/(\w+)/
 	const id = line.match(regexId);
 	let todoId: string;
 	if (id != null) {
-		todoId = id[1];	
+		todoId = id[1];
 	} else {
 		todoId = ''
 	}
@@ -90,36 +64,31 @@ function extractTarget(line: string) {
 	const status = line.match(regexStatus)
 	let afterStatus: string;
 	if (status && status[1] == ' ') {
-		afterStatus = 'true'
+		afterStatus = 'markComplete'
 	} else {
-		afterStatus = 'false'
+		afterStatus = 'markIncomplete'
 	}
 
 	return  {todoId, afterStatus}
 }
 
 function createTodo(todo: TodoInfo, deepLink: string){
-	const url = `things:///add?title=${todo.title}&notes=${deepLink}&when=${todo.date}&x-success=obsidian://things-sync-id&tags=${todo.tags}`;
+	const url = `omnifocus://x-callback-url/add?name=${todo.title}&note=${deepLink}&defer=${todo.date}&autosave=true&x-success=obsidian://of-sync-id`;
 	window.open(url);
 }
 
-function updateTodo(todoId: string, completed: string, authToken: string){
-	const url = `things:///update?id=${todoId}&completed=${completed}&auth-token=${authToken}`;
+function updateTodo(todoId: string, completed: string){
+	const url = `omnifocus://localhost/omnijs-run?script=Task.byIdentifier%28argument%29.${completed}%28%29&arg=%22${todoId}%22`;
 	window.open(url);
 }
 
-export default class Things3Plugin extends Plugin {
-	settings: PluginSettings;
+export default class OFPlugin extends Plugin {
 
 	async onload() {
-		
-		// Setup Settings Tab
-		await this.loadSettings();
-		this.addSettingTab(new Things3SyncSettingTab(this.app, this));
 
 		// Register Protocol Handler
-		this.registerObsidianProtocolHandler("things-sync-id", async (id) => {
-			const todoID = id['x-things-id'];
+		this.registerObsidianProtocolHandler("of-sync-id", async (id) => {
+			const todoID = id['result'];
 			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
 			if (view == null) {
 				return;
@@ -140,17 +109,17 @@ export default class Things3Plugin extends Plugin {
 				}
 
 				if (firstLetterIndex > 0) {
-					view.editor.replaceRange(`[${line}](things:///show?id=${todoID})`, startRange, endRange);
+					view.editor.replaceRange(`[${line}](${todoID})`, startRange, endRange);
 				} else {
-					view.editor.replaceRange(`- [ ] [${line}](things:///show?id=${todoID})`, startRange, endRange);
+					view.editor.replaceRange(`- [ ] [${line}](${todoID})`, startRange, endRange);
 				}
 			}
 		});
-	
+
 		// Create TODO Command
 		this.addCommand({
-			id: 'create-things-todo',
-			name: 'Create Things Todo',
+			id: 'create-of-todo',
+			name: 'Create Omnifocus Todo',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				const workspace = this.app.workspace;
 				const fileTitle = workspace.getActiveFile()
@@ -162,16 +131,16 @@ export default class Things3Plugin extends Plugin {
 					const obsidianDeepLink = (this.app as any).getObsidianUrl(fileTitle)
 					const encodedLink = urlEncode(obsidianDeepLink)
 					const line = getCurrentLine(editor, view)
-					const todo = contructTodo(line, this.settings, fileName)
+					const todo = contructTodo(line, fileName)
 					createTodo(todo, encodedLink)
 				}
 			}
 		});
-		
+
 		// Toggle task status and sync to things
 		this.addCommand({
-			id: 'toggle-things-todo',
-			name: 'Toggle Things Todo',
+			id: 'toggle-of-todo',
+			name: 'Toggle Omnifocus Todo',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				const workspace = this.app.workspace;
 				const fileTitle = workspace.getActiveFile()
@@ -181,66 +150,18 @@ export default class Things3Plugin extends Plugin {
 					const line = getCurrentLine(editor, view)
 					const target = extractTarget(line)
 					if (target.todoId == '') {
-						new Notice(`This is not a things3 todo`);
+						new Notice(`This is not an Omnifocus todo`);
 					} else {
 						view.app.commands.executeCommandById("editor:toggle-checklist-status")
-						updateTodo(target.todoId, target.afterStatus, this.settings.authToken)
+						updateTodo(target.todoId, target.afterStatus)
 						new Notice(`${target.todoId} set completed:${target.afterStatus} on things3`);
 					}
-					
+
 				}
 			}
 		});
 	}
 
 	onunload() {
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class Things3SyncSettingTab extends PluginSettingTab {
-	plugin: Things3Plugin;
-
-	constructor(app: App, plugin: Things3Plugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-		containerEl.createEl('h2', {text: 'Settings for Obsidian Things3 Sync.'});
-
-		new Setting(containerEl)
-			.setName('Auth Token')
-			.setDesc('Require Things3 Auth Token for syncing Todo status; Get Auth Token\
-			via Things3 -> Preferece -> General -> Enable things URL -> Manage.')
-			.addText(text => text
-				.setPlaceholder('Leave Things3 Auth Token here')
-				.setValue(this.plugin.settings.authToken)
-				.onChange(async (value) => {
-					this.plugin.settings.authToken = value;
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('Default Tags')
-			.setDesc('The default tags for Obsidian Todo; Using comma(,) \
-			to separate multiple tags; Leave this in blank for no default tags')
-			.addText(text => text
-				.setPlaceholder('Leave your tags here')
-				.setValue(this.plugin.settings.defaultTags)
-				.onChange(async (value) => {
-					this.plugin.settings.defaultTags = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
